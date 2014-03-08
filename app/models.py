b348b8a997620702886d340e6705f775b7ec56b9
Manav Goel <manav@manavgoel.net>
2014-03-08 16:48:14 +0530

    Initialized User, Songs, Playlist models.

---
+++ app/models.py
@@ -0,0 +1,23 @@
+from app import db
+
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    password = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+    playlist = db.relationship('Playlist', backref='list_contents', lazy='dynamic')
+
+
+class Songs(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    songs = db.relationship('Playlist', backref='song_list', lazy='dynamic')
+
+class Playlist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    songs_id = db.Column(db.Integer, db.ForeignKey('songs.id'))
